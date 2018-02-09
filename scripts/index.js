fa616e3dbc978e218e093608e31b62d7069a1a3d
Eric Trinchero <eric.trinchero.18@gmail.com>
2018-02-09 10:47:01 -0300

    Script to npm

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var download = require('download-github-repo'),
+    program = require('commander');
+
+program
+ .version('0.1.0')
+ .option('-f, --folder [type]')
+ .parse(process.argv);
+
+let folder = program.folder ? program.folder : './react-redux-boilerplate'
+
+
+download('trinchero18/react-redux-boilerplate', folder, function(err){
+    if(err)
+    console.log(err)
+})
