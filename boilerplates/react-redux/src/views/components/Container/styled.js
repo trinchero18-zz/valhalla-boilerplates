6a9d00e6c43f2515c527433e2a5486e125505989
Eric Trinchero <eric.trinchero.18@gmail.com>
2018-06-15 16:23:16 -0300

    updating images

--- boilerplates/react-redux/src/views/components/Container/styled.js
+++ boilerplates/react-redux/src/views/components/Container/styled.js
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 export const ContainerStyled = styled.div`
     align-items: center;
     display: flex;
-    height: calc(100vh - 80px);
+    height: 100vh;
     justify-content: center;
     overflow-x: hidden;
     overflow-y: auto;
     width: 100%;
+
+    img {
+        width: 35%;
+    }
 `;
 
 ContainerStyled.displayName = 'ContainerStyled';
